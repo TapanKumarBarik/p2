44b85f6eabb7cf2f7d8a70e58603b166e08a1a51
Tapan Barik <tapankumarbarik7@gmail.com>
2026-09-12 11:00:52 +0530

    Add implementation and README.md for Pattern 8 with examples

---
+++ code/Build-up Logical Thinking/Easy and Medium/Pattern 8/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pattern8(self, n):
+        for i in range(n):
+            for j in range(2 * n):
+                if j < i:
+                    print(" ", end="")
+                elif j < 2 * n - i - 1:
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print()
+            
+sol = Solution()
+n_list = [5, 3, 70,100,111]
+for n in n_list:
+    print(f"Pattern for n={n}:")
+    sol.pattern8(n)
+    print()
